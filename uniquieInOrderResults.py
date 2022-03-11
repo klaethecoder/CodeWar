77b8e27fc4029d8374869a3686208bb79c1392d5
Ryan Richard <klaethecoder+Gen1@gmail.com>
2022-03-11 13:11:17 -0700

    I was able to figure this one out with a little help and get it working. Also saved a couple example solutions.

--- uniquieInOrderResults.py
+++ uniquieInOrderResults.py
@@ -3,7 +3,13 @@
 # without any elements with the same value next to each other and preserving the original order of elements.
 
 def unique_in_order(iterable):
-    pass
+    new_list = []
+    for letter in iterable:
+        if new_list == []:
+            new_list.append(letter)
+        elif new_list[-1] != letter:
+            new_list.append(letter)
+    return new_list
 
 print(unique_in_order('AAAABBBCCDAABBB'))
 
@@ -15,3 +21,10 @@ print(unique_in_order('AAAABBBCCDAABBB'))
 # unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 # unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 # unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+# Solution 1:
+# def unique_in_order(iterable):
+#     new_list = []
+#     for letter in iterable:
+#         letter in new_list[-1:] or new_list.append(letter)
+#     return new_list

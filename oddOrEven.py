89d694081a70139138ac96b092997045b48b3e4d
Ryan Richard <klaethecoder+Gen1@gmail.com>
2022-03-11 08:24:22 -0700

    Initial Commit for theses Kata

---
+++ oddOrEven.py
@@ -0,0 +1,24 @@
+# Task:
+# Given a list of integers, determine whether the sum of its elements is odd or even.
+
+# Give your answer as a string matching "odd" or "even".
+
+# If the input array is empty consider it as: [0] (array with a zero).
+
+# 7kyu Example:
+
+def odd_or_even(arr):
+    pass
+
+
+# Testing:
+# import codewars_test as test
+# from solution import odd_or_even
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(odd_or_even([0, 1, 2]), "odd")
+#         test.assert_equals(odd_or_even([0, 1, 3]), "even")
+#         test.assert_equals(odd_or_even([1023, 1, 2]), "even")
---
+++ uniquieInOrder.py
@@ -0,0 +1,16 @@
+# Instructions:
+# Implement the function unique_in_order which takes as argument a sequence and returns a list of items 
+# without any elements with the same value next to each other and preserving the original order of elements.
+
+def unique_in_order(iterable):
+    pass
+
+
+
+# Testing:
+# test.assert_equals(unique_in_order('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+
+# Examples:
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]

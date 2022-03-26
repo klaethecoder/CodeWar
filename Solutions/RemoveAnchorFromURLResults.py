17d70ef5324c5e53945e04d4ae8be0e98e6d723f
klaethecoder <klaethecoder@gmail.com>
2022-03-26 12:16:59 -0600

    I was able to work through the issues and make sure to get a correct Answer.

--- Solutions/RemoveAnchorFromURLResults.py
+++ Solutions/RemoveAnchorFromURLResults.py
@@ -18,3 +18,23 @@ def remove_url_anchor(url):
 #         test.assert_equals(remove_url_anchor("www.codewars.com#about"), "www.codewars.com")
 #         test.assert_equals(remove_url_anchor("www.codewars.com/katas/?page=1#about"), "www.codewars.com/katas/?page=1")
 #         test.assert_equals(remove_url_anchor("www.codewars.com/katas/"), "www.codewars.com/katas/")
+
+# Solutions:
+
+# Solition 1:
+# def remove_url_anchor(url):
+    # index = url.find('#')
+    # return url[:index] if index >= 0 else url
+
+# Solution 2:
+# def remove_url_anchor(url):
+#   import re
+#   return re.sub('#.*$','',url)
+
+# Solution 3:
+# def remove_url_anchor(url):
+#   return url.partition('#')[0]
+
+# Solution 4:
+# def remove_url_anchor(url):
+#   return url.split('#')[0]

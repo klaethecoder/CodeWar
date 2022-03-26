b61e9318719e51b2db549e87ea32490c380a8ba7
klaethecoder <klaethecoder@gmail.com>
2022-03-26 12:17:37 -0600

    Still working through these issues. Will come back to them.

---
+++ Solutions/ipValidationResults.py
@@ -0,0 +1,40 @@
+# Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if 
+# they consist of four octets, with values between 0 and 255, inclusive.
+
+# Valid inputs examples:
+# Examples of valid inputs:
+# 1.2.3.4
+# 123.45.67.89
+# Invalid input examples:
+# 1.2.3
+# 1.2.3.4.5
+# 123.456.78.90
+# 123.045.067.089
+# Notes:
+# Leading zeros (e.g. 01.02.03.04) are considered invalid
+# Inputs are guaranteed to be a single string
+
+# Kata 6kyu
+
+def is_valid_IP(strng):
+    return None
+
+# import codewars_test as test
+# from solution import is_valid_IP
+
+# @test.describe("Sample tests")
+# def _():
+#     @test.it("Tests")
+#     def __():
+#         test.assert_equals(is_valid_IP('12.255.56.1'),     True)
+#         test.assert_equals(is_valid_IP(''),                False)
+#         test.assert_equals(is_valid_IP('abc.def.ghi.jkl'), False)
+#         test.assert_equals(is_valid_IP('123.456.789.0'),   False)
+#         test.assert_equals(is_valid_IP('12.34.56'),        False)
+#         test.assert_equals(is_valid_IP('12.34.56 .1'),     False)
+#         test.assert_equals(is_valid_IP('12.34.56.-1'),     False)
+#         test.assert_equals(is_valid_IP('123.045.067.089'), False)
+#         test.assert_equals(is_valid_IP('127.1.1.0'),        True)
+#         test.assert_equals(is_valid_IP('0.0.0.0'),          True)
+#         test.assert_equals(is_valid_IP('0.34.82.53'),       True)
+#         test.assert_equals(is_valid_IP('192.168.1.300'),   False)
